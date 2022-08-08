dd094d6984b355972873349bad384f0b575b97a4
Niv Kaufman <nivkman@gmail.com>
2022-08-08 23:58:40 +0300

    fixing port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 require('dotenv').config();
 app.use(cors());
@@ -27,6 +26,8 @@ app.use(bodyParser.json());
 app.use('/api/v1', require('./routers/v1.js'));
 
 app.get('/:id', (req, res) => res.redirect(`/api/v1/${req.params.id}`));
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`(backend) running at PORT: ${PORT}`);
